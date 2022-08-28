39ab7717ba25b67ff4289e41fd38cd8289114bb6
felipe <andresfelram02@gmail.com>
2022-08-28 16:54:17 -0400

    Guardar

--- notas.py
+++ notas.py
@@ -1,16 +1,53 @@
 # Notas de la clase 
 # Siempre debemos crear lo siguente al iniciar un nuevo proyecto: ""
+import random
 def run():
-    pass
+    
+    
+    #esto hace print solo pares
     # for contador in range(100):
     #       if contador % 2 != 0:
     #           continue
     #       print(contador)
         
-#esto hace print solo pares
-                    
-       
-        
+
+
+    #esto es un juego de python
+
+
+
+    #numero_aleatorio = random.randint(1,100)
+    #numero_elegido = int(input("elige un numero del 1 al 100: "))
+    #while numero_elegido != numero_aleatorio:
+    #   if numero_elegido < numero_aleatorio:
+    #       print("Busca un numero mas grande")
+    #    else:
+    #        print("Busca un numero mas pequeno")
+    #        numero_elegido = int(input("elige otro numero "))
+    #    numero_elegido = int(input("elige otro numero "))
+    #print("Ganaste")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 if __name__ == "__main__":
     run()
     
@@ -20,9 +57,3 @@ if __name__ == "__main__":
     
     
     #git push -u origin main
-#
-#
-#
-#
-#
-#
