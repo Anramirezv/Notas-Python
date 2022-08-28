ef60b4f989903a82ebf2653bcafcaadad4c94e03
felipe <andresfelram02@gmail.com>
2022-08-28 13:45:01 -0400

    Notas clase 1

---
+++ notas.py
@@ -0,0 +1,22 @@
+# Notas de la clase 
+# Siempre debemos crear lo siguente al iniciar un nuevo proyecto: ""
+def run():
+    pass
+    # for contador in range(100):
+    #       if contador % 2 != 0:
+    #           continue
+    #       print(contador)
+        
+#esto hace print solo pares
+                    
+       
+        
+if __name__ == "__main__":
+    run()
+
+#
+#
+#
+#
+#
+#
